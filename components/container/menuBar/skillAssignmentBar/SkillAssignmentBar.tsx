698c1be1ea63a78aa861d02939c5a3153383c5eb
thumbthing <ilscc0902@gmail.com>
2026-03-31 22:35:54 +0900

    feat-design(menuBar): 테이블 확인 메뉴바 기능 구현, 스타일링
    -feat
    특정 테이블 위치 이동 기능 개선(최대 입력값 설정)
    navigate 기능 제거

--- components/container/menuBar/skillAssignmentBar/SkillAssignmentBar.tsx
+++ components/container/menuBar/skillAssignmentBar/SkillAssignmentBar.tsx
@@ -1,18 +1,14 @@
 "use client";
 
 import { useAppDispatch, useAppSelector } from "@/app/hooks";
-import { decreaseCount, increaseCount, setCount } from "@/feature/store/slices/table/tableSlice";
+import { decreaseCount, increaseCount, setCount, setIsRecordListOnView } from "@/feature/store/slices/table/tableSlice";
 import style from "./SkillAssignmentBar.module.css"
-import TarredDeviceMatchBox from "./TarredDeviceMatchBox";
-import { useRouter } from "next/navigation";
-
-type RouteKeyType = "advance-setting" | "table-check";
+import NavigateTableCheckButton from "@/components/button/navigate/NavigateTableCheckButton";
 
 export default function SkillAssignmentBar() {
   const dispatch = useAppDispatch();
-  const router = useRouter()
-
-  const advanceParam = useAppSelector(state => state.urlParam.advanceSettingParam);
+  const recordList = useAppSelector(state => state.table.tableRecordList);
+  const maxInput = recordList.at(-1)?.order;
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -21,32 +17,25 @@ export default function SkillAssignmentBar() {
     dispatch(setCount(inputCount))
   }
 
-  const navigateToAdvanceSetting = (routeKey: RouteKeyType) => {
-    const location = `${window.location.origin}/${routeKey}?${advanceParam}`
-    router.push(location);
-  }
-
   return (
     <div className={style.menuBar}>
-      <div className={style.navigateBox}>
-        <button onClick={() => navigateToAdvanceSetting("advance-setting")}>격화 세팅 변경</button>
-        <button onClick={() => navigateToAdvanceSetting("table-check")} disabled={advanceParam === ""}>테이블 기록</button>
-      </div>
-      <div className="tarredDeviceBox">
-        <TarredDeviceMatchBox />
-      </div>
-      <div>
-      </div>
+      <NavigateTableCheckButton />
+      <div className={style.tableListButton} onClick={() => dispatch(setIsRecordListOnView(true))}>전체 테이블 기록 확인</div>
+
       <div className={style.tableCountContainer}>
-        <form onSubmit={(e) => handleSubmit(e)}>
-          <p>
-            테이블 이동 :
-            <input type="number" name="count"/>
-          </p>
-          <button type="submit">enter</button>
-        </form>
-        <button className={style.button} onClick={() => dispatch(decreaseCount())}>이전 테이블</button>
-        <button className={style.button} onClick={() => dispatch(increaseCount())}>다음 테이블</button>
+        <div>
+          <form className={style.tableMoveInputForm} onSubmit={(e) => handleSubmit(e)}>
+            <p className={style.tableMoveInputText}>
+              테이블 이동 :
+              <input type="number" min="0" max={maxInput} className={style.tableMoveInput} name="count"/>
+            </p>
+            <button type="submit" className={style.tableMoveSubmitButton}>이동</button>
+          </form>
+        </div>
+        <div className={style.countButtonBox}>
+          <button className={style.countButton} onClick={() => dispatch(decreaseCount())}>이전 테이블</button>
+          <button className={style.countButton} onClick={() => dispatch(increaseCount())}>다음 테이블</button>
+        </div>
       </div>
     </div>
   )
