a1c55507d43288c3063f3d5b5b900bc66f3d2b20
thumbthing <ilscc0902@gmail.com>
2026-03-31 21:53:27 +0900

    feat-style(advanceSetting): navigation 컴포넌트 제거, 스타일 추가
    - style
    flex에서 grid로 템플릿 생성
    hover 스타일 추가

--- components/weapon/WeaponSelectBox.tsx
+++ components/weapon/WeaponSelectBox.tsx
@@ -5,7 +5,6 @@ import { useAppDispatch, useAppSelector } from "@/app/hooks";
 import { addWeapon } from "@/feature/store/slices/weapon/weaponSlice";
 import style from "./WeaponSelectBox.module.css";
 import { WeaponType } from "@/global/type/appType";
-import NavigateTableCheckButton from "../button/navigate/NavigateTableCheckButton";
 
 // TODO: 배열 형태 push, pop으로는 리렌더가 일어나지 않는 이유 정리
 export default function WeaponSelectBox() {
@@ -22,20 +21,15 @@ export default function WeaponSelectBox() {
 
   return (
     <div className={style.container}>
-      <div className={style.selectContainer}>
-        <div className={style.selectHeader}>
-          <div>
-            무기 목록
+      <div className={style.selectHeader}>
+        <p className={style.headerText}>격화 효율 설정</p>
+      </div>
+      <div className={style.selectListBox}>
+        {WEAPON_LIST.map(weapon => 
+          <div key={weapon} className={style.weaponName} onClick={() => addUniqueWeaponList(weapon)}>
+            {weapon}
           </div>
-          <NavigateTableCheckButton />
-        </div>
-        <div className={style.selectListBox}>
-          {WEAPON_LIST.map(weapon => 
-            <div key={weapon} onClick={() => addUniqueWeaponList(weapon)}>
-              {weapon}
-            </div>
-          )}
-        </div>
+        )}
       </div>
     </div>
   )
