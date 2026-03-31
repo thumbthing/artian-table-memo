e938a2d8b3db804acf0f3fce2eab60a88e144aab
thumbthing <ilscc0902@gmail.com>
2026-03-31 21:43:47 +0900

    feat(store): 더미 데이터 삭제, portal state 추가, reducer 추가, state 초기화 기능 개선
    - portal
    테이블 전체 기록용 state 추가
    
    - reducer
    portal state를 payload로 최신화
    테이블 전체 기록 state 변경 기능 추가
    테이블 기록 삭제 기능 추가
    state 초기화 개선

--- feature/store/slices/table/tableSlice.ts
+++ feature/store/slices/table/tableSlice.ts
@@ -1,5 +1,4 @@
 import { RootState } from "@/feature/store/store";
-import { SORTED_DUMI_DATA } from "@/global/data/dumiData";
 import { ElementType, WeaponType } from "@/global/type/appType";
 import { TableRecordType, TableStateType } from "@/global/type/extendedType";
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
@@ -16,6 +15,7 @@ interface GetDefaultStateActionType extends SetWeaponElementActionType {
 interface ExtendedTableStateType extends TableStateType {
   isSeriesSkillSelecting: boolean,
   isGroupSkillSelecting: boolean,
+  isRecordListOnView: boolean,
   seriesSkill: string,
   groupSkill: string,
 }
@@ -24,12 +24,12 @@ const initialState: ExtendedTableStateType = {
   isOnRecord: false,
   isSeriesSkillSelecting: false,
   isGroupSkillSelecting: false,
+  isRecordListOnView: false,
   seriesSkill: "",
   groupSkill: "",
   weaponName: undefined,
   elementName: undefined,
   tableRecordList: [],
-  // tableRecordList: SORTED_DUMI_DATA,
   count: 0
 }
 
@@ -41,69 +41,98 @@ export const tableSlice = createSlice({
       if (state.isOnRecord === action.payload) return;
       state.isOnRecord = !state.isOnRecord;
     },
+
     setSeriesSkillSelecting: (state, action: PayloadAction<boolean>) => {
       if (state.isSeriesSkillSelecting === action.payload) return;
-      state.isSeriesSkillSelecting = !state.isGroupSkillSelecting;
+      state.isSeriesSkillSelecting = action.payload;
     },
+    
     setGroupSkillSelecting: (state, action: PayloadAction<boolean>) => {
       if (state.isGroupSkillSelecting === action.payload) return;
-      state.isGroupSkillSelecting = !state.isGroupSkillSelecting;
+      state.isGroupSkillSelecting = action.payload;
     },
+
+    setIsRecordListOnView: (state, action: PayloadAction<boolean>) => {
+      if (state.isRecordListOnView === action.payload) return;
+      state.isRecordListOnView = action.payload;
+    },
+
     setWeaponElement: (state, action: PayloadAction<SetWeaponElementActionType>) => {
       state.weaponName = action.payload.weaponName;
       state.elementName = action.payload.elementName;
     },
+
     setSeriesSkill: (state, action: PayloadAction<string>) => {
       state.seriesSkill = action.payload;
       state.isSeriesSkillSelecting = false;
     },
+
     setGroupSkill: (state, action: PayloadAction<string>) => {
       state.groupSkill = action.payload;
       state.isGroupSkillSelecting = false;
     },
+
     resetRecordState: (state) => {
       state.isOnRecord = false;
       state.weaponName = undefined;
       state.elementName = undefined;
-    },
-    resetAllSeriesSkillState: (state) => {
-      state.isSeriesSkillSelecting = false;
       state.seriesSkill = "";
-    },
-    resetAllGroupSkillState: (state) => {
-      state.isGroupSkillSelecting = false;
       state.groupSkill = "";
     },
+
+    resetAllSeriesSkillState: (state) => {
+      state.seriesSkill = "";
+      state.isSeriesSkillSelecting = false;
+    },
+
+    resetAllGroupSkillState: (state) => {
+      state.groupSkill = "";
+      state.isGroupSkillSelecting = false;
+    },
+
     getDefaultRecordState: (state, action: PayloadAction<GetDefaultStateActionType>) => {
       state.isOnRecord = action.payload.isOnRecord;
       state.weaponName = action.payload.weaponName;
       state.elementName = action.payload.elementName;
     },
+
     addTableRecord: (state, action: PayloadAction<TableRecordType>) => {
       state.isOnRecord = false;
       state.weaponName = undefined;
       state.elementName = undefined;
+      state.seriesSkill = "";
+      state.groupSkill = "";
       state.tableRecordList = state.tableRecordList.concat(action.payload);
     },
+
+    removeTableRecord: (state, action: PayloadAction<number>) => {
+      state.tableRecordList = state.tableRecordList.toSpliced(action.payload, 1);
+    },
+
     sortTableRecord: (state) => {
       state.tableRecordList = [...state.tableRecordList].sort((a, b) => a.order - b.order);
     },
+
     increaseCount: (state) => {
       state.count = state.count + 1;
     },
+
     decreaseCount: (state) => {
       if (state.count > 0) {
         state.count = state.count - 1;
       }
     },
+
     setCount: (state, action: PayloadAction<number>) => {
       state.count = action.payload;
     },
+
     resetTableState: (state) => {
       return {
         isOnRecord: false,
         isSeriesSkillSelecting: false,
         isGroupSkillSelecting: false,
+        isRecordListOnView: false,
         seriesSkill: "",
         groupSkill: "",
         elementName: undefined,
@@ -119,6 +148,7 @@ export const {
   setOnRecord, 
   setSeriesSkillSelecting,
   setGroupSkillSelecting,
+  setIsRecordListOnView,
   setWeaponElement, 
   setSeriesSkill,
   setGroupSkill,
@@ -127,6 +157,7 @@ export const {
   resetAllGroupSkillState,
   getDefaultRecordState, 
   addTableRecord, 
+  removeTableRecord,
   sortTableRecord, 
   increaseCount, 
   decreaseCount, 
