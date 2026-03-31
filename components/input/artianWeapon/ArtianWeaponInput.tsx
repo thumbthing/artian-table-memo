2b121469eafa2bb63e892bb0a9b1fca1df107672
thumbthing <ilscc0902@gmail.com>
2026-03-31 22:52:26 +0900

    feat-design(artianCreate): 아티어 생성 메모용 입력 기능 구현, 스타일링
    - feat
    숫자 입력을 문자열로 변환

---
+++ components/input/artianWeapon/ArtianWeaponInput.tsx
@@ -0,0 +1,50 @@
+'use client';
+
+import { ChangeEvent, useState } from "react";
+import style from "./ArtianWeaponInput.module.css"
+
+
+interface ArtianWeaponInputType {
+  index: number
+}
+
+export default function ArtianWeaponInput ({index}: ArtianWeaponInputType) {
+  const [input, setInput] = useState<string>("");
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    let addString = "";
+    const lastInput = e.target.value.at(-1);
+
+    if (lastInput === undefined) return;
+
+    switch(lastInput) {
+      case "1" : {
+        addString = "예";
+        break;
+      }
+      case "2" : {
+        addString = "기";
+        break;
+      }
+      case "3" : {
+        addString = "회";
+        break;
+      }
+      case "4" : {
+        addString = "속"
+        break
+      }
+    }
+
+    if (addString === "") return;
+
+    setInput(`${input}${addString}`)
+  }
+
+  return (
+    <div className={style.inputBox}>
+      <div className={style.order}>{index + 1}</div>
+      <input type="text" className={style.inputArea} onChange={(e) => handleChange(e)} maxLength={5} value={input}/>
+    </div>
+  )
+}
