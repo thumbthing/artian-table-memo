90d08c02159c22437b9ecb92c0bd1d99ef3022d5
thumbthing <ilscc0902@gmail.com>
2026-03-31 22:00:30 +0900

    feat-style(tableCheck): 테이블 기록 삭제 기능 추가, 스타일링

--- components/table/TableRecordList.tsx
+++ components/table/TableRecordList.tsx
@@ -1,35 +1,45 @@
 "use client";
 
-import { useAppSelector } from "@/app/hooks";
+import { useAppDispatch, useAppSelector } from "@/app/hooks";
 import style from "./TableRecordList.module.css"
+import { removeTableRecord } from "@/feature/store/slices/table/tableSlice";
+import { Fragment } from "react/jsx-runtime";
 
 export default function TableRecordList() {
   const recordList = useAppSelector(state => state.table.tableRecordList);
+  const dispatch = useAppDispatch();
+
   return (
     <div className={style.box}>
-      <div>
-        <p>테이블 기록</p>
+      <div className={style.recordListHeader}>
+        <h3>테이블 기록</h3>
+        <p>삭제할 기록은 클릭으로 삭제</p>
       </div>
-      <div>
+      <div className={style.recordBox}>
         {recordList.map((record, index) => {
           return (
-            <div className={style.record} key={`record-${record.weaponName}-${record.elementName}-${record.order}-${index}`}>
-              <div className={style.weaponTag}>
-                <p>
-                  {record.weaponName}
-                </p>
-                <p>
-                  {record.elementName}속성
-              </p>
-              </div>
-              <div>
-                <p>제작 횟수 : {record.order}</p>
-              </div>
-              <div className={style.skillTag}>
-                <p>{record.seriesSkill}</p>
-                <p>{record.groupSkill}</p>
-              </div>
-            </div>
+            <Fragment key={`record-${record.weaponName}-${record.elementName}-${record.order}-${index}`}>
+              <label onClick={() => dispatch(removeTableRecord(index))}>
+                <div className={style.record}>
+                  <div className={style.recordInfoBox}>
+                    <div className={style.weaponTag}>
+                      <p>{record.weaponName}</p>
+                      <p>{record.elementName}속성</p>
+                    </div>
+                    <div className={style.orderTag}>
+                      <p>테이블</p>
+                      <p>{record.order}</p>
+                    </div>
+                  </div>
+                  <div className={style.skillTag}>
+                    <div className={style.skillRecord}>
+                      <p>{record.seriesSkill}</p>
+                      <p>{record.groupSkill}</p>
+                    </div>
+                  </div>
+                </div>
+              </label>
+            </Fragment>
           )
         })}
       </div>
