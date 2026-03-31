20dbff3d3c83592578d210fd910ce9e297e51483
thumbthing <ilscc0902@gmail.com>
2026-03-31 22:50:50 +0900

    feat-design(menuBar): 내비게이션 메뉴바 기능 구현, 스타일링

--- components/container/menuBar/navigationBar/NavigationBar.tsx
+++ components/container/menuBar/navigationBar/NavigationBar.tsx
@@ -1,28 +1,38 @@
 "use client";
 
 import { ROUTE } from "@/global/data/routeData";
-import { useRouter } from "next/router";
-import { useRef, useState } from "react";
+import Link from "next/link";
+import style from "./NavigationBar.module.css"
 
 export default function NavigationBar() {
-  const router = useRouter();
-
-  const locationRef = useRef<string>(window.location.origin);
-
-  const [appRoute, setAppRoute] = useState(
-  {
-    home: `${locationRef.current}${ROUTE.home}`,
-    advanceSetting: `${locationRef.current}${ROUTE.advanceSetting}`,
-    tableCheck: `${locationRef.current}${ROUTE.tableCheck}`,
-    skillAssignment: `${locationRef.current}${ROUTE.skillAssignment}`
-  });
 
   return (
-    <div>
-      <div onClick={() => {router.push(appRoute.home)}}>홈</div>
-      <div onClick={() => {router.push(appRoute.advanceSetting)}}>격화 세팅</div>
-      <div onClick={() => {router.push(appRoute.tableCheck)}}>테이블 확인</div>
-      <div onClick={() => {router.push(appRoute.skillAssignment)}}>스킬 부여</div>
-    </div>
+    <article className={style.menuBar}>
+      <nav className={style.nav}>
+        <Link href={ROUTE.home} className={style.link}>
+          메인
+        </Link>
+      </nav>
+      <nav className={style.nav}>
+        <Link href={ROUTE.artianCreate} className={style.link}>
+          아티어
+        </Link>
+      </nav>
+      <nav className={style.nav}>
+        <Link href={ROUTE.advanceSetting} className={style.link}>
+          격화 세팅
+        </Link>
+      </nav>
+      <nav className={style.nav}>
+        <Link href={ROUTE.tableCheck} className={style.link}>
+          테이블 확인
+        </Link>
+      </nav>
+      <nav className={style.nav}>
+        <Link href={ROUTE.skillAssignment} className={style.link}>
+          스킬 재부여
+        </Link>
+      </nav>
+    </article>
   )
 }
