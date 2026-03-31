fe0a70d73fcb8cfffa56f59c692e71f8fe5c2741
thumbthing <ilscc0902@gmail.com>
2026-03-31 22:07:51 +0900

    feat-style-design(skillAssignment): 스타일링, url 버그 수정

--- components/skillAssignment/NoMoreRecord.tsx
+++ components/skillAssignment/NoMoreRecord.tsx
@@ -4,6 +4,7 @@ import { useAppDispatch, useAppSelector } from "@/app/hooks";
 import { resetAllTarredTableState } from "@/feature/store/thunks/resetAllTarredTableState";
 import { ROUTE } from "@/global/data/routeData";
 import { useRouter } from "next/navigation";
+import style from "./NoMoreRecord.module.css"
 
 export default function NoMoreRecord() {
   const router = useRouter();
@@ -12,18 +13,14 @@ export default function NoMoreRecord() {
 
   const handleOnClick = () => {
     dispatch(resetAllTarredTableState());
-    const tableCheckLocation = `${window.location.origin}${ROUTE.tableCheck}${advanceParam}`
+    const tableCheckLocation = `${window.location.origin}${ROUTE.tableCheck}?${advanceParam}`
     router.push(tableCheckLocation);
   }
 
   return (
-    <div>
-      <div>
-        <p>이후의 테이블에 유효한 스킬이 존재하지 않습니다.</p>
-      </div>
-      <div>
-        <button onClick={() => handleOnClick()}>테이블 기록으로 이동</button>
-      </div>
+    <div className={style.box}>
+      <p>이후의 테이블에 유효한 스킬이 존재하지 않습니다.</p>
+      <button onClick={() => handleOnClick()}>테이블 기록으로</button>
     </div>
   )
 }
