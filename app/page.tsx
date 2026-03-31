3ea5c91f0791d6e1310cf0ea46cee37c3852754f
thumbthing <ilscc0902@gmail.com>
2026-03-31 21:22:44 +0900

    feat: root 페이지 기능 추가
    - global css 초기화, 페이지 라우팅, 네비게이션 바 추가

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,25 @@
 import NavigateAdvanceSettingButton from "@/components/button/navigate/NavigateAdvanceSettingButton";
 import { store } from "@/feature/store/store";
 import { Provider } from "react-redux";
+import style from "./page.module.css";
+import NavigateArtianCreateButton from "@/components/button/navigate/NavigateArtianCreateButton";
+import NavigationBar from "@/components/container/menuBar/navigationBar/NavigationBar";
 
 export default function Home() {
   return (
     <Provider store={store}>
-      <div>
-        <div>
-          <p>거극 아티어 격화 세팅</p>
-          <NavigateAdvanceSettingButton />
+      <NavigationBar />
+      <div className={style.container}>
+        <div className={style.header}>아티어 제작 메모</div>
+        <div className={style.navigateBox}>
+          <div className={style.atianNavigateButton}>
+            <p>아티어 무기 제작</p>
+            <NavigateArtianCreateButton />
+          </div>
+          <div className={style.gogmaziosArtianNavigateButton}>
+            <p>거극 아티어 스킬</p>
+            <NavigateAdvanceSettingButton />
+          </div>
         </div>
       </div>
     </Provider>
