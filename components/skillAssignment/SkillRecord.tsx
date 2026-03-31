7000f87828c432e6e4e2c810267420397f065b9b
thumbthing <ilscc0902@gmail.com>
2026-03-31 22:06:14 +0900

    design-style(skillAssignment): 유효 스킬 컴포넌트 스타일링

--- components/skillAssignment/SkillRecord.tsx
+++ components/skillAssignment/SkillRecord.tsx
@@ -9,23 +9,12 @@ export default function SkillRecord({record}: SkillRecordPropsType) {
   return (
     <div className={style.box}>
       <div className={style.weaponTag}>
-        <div>
-          <p>무기 :</p>
-          <p>속성 :</p>
-        </div>
-        <div>
-          <p>{record.weaponName}</p>
-          <p>{record.elementName}속성</p>
-        </div>
+        <div>{record.weaponName}</div>
+        <div>{record.elementName} 속성</div>
       </div>
       <div className={style.skillTag}>
-        <div>
-          <p>스킬</p>
-        </div>
-        <div className={style.skillRecord}>
-          <p>{record.seriesSkill}</p>
-          <p>{record.groupSkill}</p>
-        </div>
+        <div>{record.seriesSkill}</div>
+        <div>{record.groupSkill}</div>
       </div>
     </div>
   )
