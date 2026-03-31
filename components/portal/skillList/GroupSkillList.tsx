876b8cbb425cc5d7923b73b3d47a18d7ca881b86
thumbthing <ilscc0902@gmail.com>
2026-03-31 22:17:15 +0900

    design-style(portal): 공통 css 파일로 스타일 적용

--- components/portal/skillList/GroupSkillList.tsx
+++ components/portal/skillList/GroupSkillList.tsx
@@ -4,31 +4,31 @@ import { useAppDispatch } from "@/app/hooks";
 import { resetAllGroupSkillState, setGroupSkill } from "@/feature/store/slices/table/tableSlice";
 import { ORDERED_GROUP_SKILL } from "@/global/data/skillData";
 import { createPortal } from "react-dom";
-import style from "./GroupSkillList.module.css"
+import style from "./SeriesGroupSkillList.module.css"
 
 export default function GroupSkillList() {
   const dispatch = useAppDispatch()
 
-  const closeProtal = () => {
+  const closePortal = () => {
     dispatch(resetAllGroupSkillState());
     close
   }
 
   return createPortal(
-    <div className={style.overlay} onClick={() => closeProtal()}>
-      <div 
-        className={style.skillListBox}
-        onClick={(e) => e.stopPropagation()}
-        >
-      {ORDERED_GROUP_SKILL.map(skill => 
-        <label key={skill}>
-          <p onClick={() => {
-            dispatch(setGroupSkill(skill))
-          }}>
-            {skill}
-          </p>
-        </label>
-      )}
+    <div className={style.overlay} onClick={() => closePortal()}>
+      <div className={style.portalContainer} onClick={(e) => e.stopPropagation()}>
+        <p className={style.skillListHeader}>그룹 스킬 목록</p>
+        <div className={style.skillListBox}>
+        {ORDERED_GROUP_SKILL.map(skill => 
+          <label key={skill}>
+            <div  className={style.singleSkill} onClick={() => {
+              dispatch(setGroupSkill(skill))
+            }}>
+              {skill}
+            </div>
+          </label>
+        )}
+        </div>
       </div>
     </div>
   , document.body)
--- components/portal/skillList/SeriesSkillList.tsx
+++ components/portal/skillList/SeriesSkillList.tsx
@@ -4,7 +4,7 @@ import { useAppDispatch } from "@/app/hooks";
 import { resetAllSeriesSkillState, setSeriesSkill } from "@/feature/store/slices/table/tableSlice";
 import { ORDERED_SERIES_SKILL } from "@/global/data/skillData";
 import { createPortal } from "react-dom";
-import style from "./SeriesSkillList.module.css"
+import style from "./SeriesGroupSkillList.module.css"
 
 export default function SeriesSkillList() {
   const dispatch = useAppDispatch()
@@ -16,19 +16,19 @@ export default function SeriesSkillList() {
 
   return createPortal(
     <div className={style.overlay} onClick={() => closeProtal()}>
-      <div 
-        className={style.skillListBox}
-        onClick={(e) => e.stopPropagation()}
-        >
-      {ORDERED_SERIES_SKILL.map(skill => 
-        <label key={skill}>
-          <p onClick={() => {
-            dispatch(setSeriesSkill(skill))
-          }}>
-            {skill}
-          </p>
-        </label>
-      )}
+      <div className={style.portalContainer} onClick={(e) => e.stopPropagation()}>
+        <p className={style.skillListHeader}>시리즈 스킬 목록</p>
+        <div className={style.skillListBox}>
+        {ORDERED_SERIES_SKILL.map(skill => 
+          <label key={skill}>
+            <div className={style.singleSkill} onClick={() => {
+              dispatch(setSeriesSkill(skill))
+            }}>
+              {skill}
+            </div>
+          </label>
+        )}
+        </div>
       </div>
     </div>
   , document.body)
