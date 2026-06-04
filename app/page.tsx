4e56a750a34c6469965238656b00b65cdc0ac61c
thumbthing <ilscc0902@gmail.com>
2026-06-04 18:07:48 +0900

    design(main): route button 스타일 수정

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,26 @@
 'use client';
 
-import NavigateAdvanceSettingButton from "@/components/button/navigate/NavigateAdvanceSettingButton";
 import { store } from "@/feature/store/store";
 import { Provider } from "react-redux";
 import style from "./page.module.css";
-import NavigateArtianCreateButton from "@/components/button/navigate/NavigateArtianCreateButton";
 import NavigationBar from "@/components/container/menuBar/navigationBar/NavigationBar";
+import useRouterPush from "@/feature/hook/useRouterPush";
+import { ROUTE } from "@/global/data/routeData";
 
 export default function Home() {
+  const routerPush = useRouterPush();
+
   return (
     <Provider store={store}>
       <NavigationBar />
       <div className={style.container}>
         <div className={style.header}>아티어 제작 메모</div>
         <div className={style.navigateBox}>
-          <div className={style.atianNavigateButton}>
+          <div className={style.atianNavigateButton} onClick={(() => {routerPush(ROUTE.artianCreate)})}>
             <p>아티어 무기 제작</p>
-            <NavigateArtianCreateButton />
           </div>
-          <div className={style.gogmaziosArtianNavigateButton}>
+          <div className={style.gogmaziosArtianNavigateButton} onClick={() => routerPush(ROUTE.advanceSetting)}>
             <p>거극 아티어 스킬</p>
-            <NavigateAdvanceSettingButton />
           </div>
         </div>
       </div>
