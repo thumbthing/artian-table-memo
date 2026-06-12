96b18b99175b8e5b912d15fef62c0bb6a2782901
thumbthing <ilscc0902@gmail.com>
2026-06-13 03:59:16 +0900

    feat(tableCheck): 테이블 기록 방식 선택 기능 추가
    - 무기 및 속성으로 입력
    기존의 모든 무기 렌더링에서 선택된 무기의 격화 효율 설정에 기반한 입력 버튼 생성으로 변경
    
    - 직접 입력
    textArea portal 관리 버튼 추가

--- components/tableCheck/WeaponTableRecordBox.tsx
+++ components/tableCheck/WeaponTableRecordBox.tsx
@@ -8,10 +8,12 @@ import { useState } from "react";
 import TableCheckCard from "./TableCheckWeaponCard";
 import { WeaponType } from "@/global/type/appType";
 import { ElementSettingType } from "@/global/type/extendedType";
+import VerifiedTableInputPortal from "../portal/tableInput/VerifiedTableInputPortal";
 
 export default function WeaponTableRecordBox() {
   const weaponList = useAppSelector(state => state.weapon.weaponList)
   const weaponSetting = useAppSelector(state => state.weapon.weaponSetting)
+  const [isTableInputVisible, setIsTableInputVisible] = useState<boolean>(false);
 
   const [selectedWeaponAdvanceSetting, setSelectedWeaponAdvanceSetting] = 
     useState<{
@@ -22,39 +24,41 @@ export default function WeaponTableRecordBox() {
   const weaponAdvanceSetting = getAdvanceWeaponObject(weaponList, weaponSetting);
   
   return (
-    <div className={style.box}>
-      <div className={style.selectedWeaponBox}>
-        {selectedWeaponAdvanceSetting !== undefined && 
-          <TableCheckCard 
-          weaponAdvanceSetting={selectedWeaponAdvanceSetting}
-          dispatchSetState={setSelectedWeaponAdvanceSetting}
-          />
-        }
-      </div>
-      <div className={style.tableRecordInputSelectBox}>
-        <div className={style.weaponListbox}>
-          {WEAPON_LIST.map((weapon, index) => 
-            <div 
-              className={`${style.weaponButton} ${weaponList.includes(weapon) ? style.advanceSet : style.advanceUnSet} ${selectedWeaponAdvanceSetting?.weapon === weapon && style.selectedWeapon}`} 
-              key={`record-select-button-${weapon}-${index}`}
-              onClick={() => setSelectedWeaponAdvanceSetting(prev => {
-                if (weaponAdvanceSetting[weapon] === undefined) return prev;
-                if (prev?.weapon === weapon) return undefined;
-                return {
-                  weapon : weapon,
-                  advanceSetting: weaponAdvanceSetting[weapon]
-                }
-              })}
-            >
-              {weapon}
-            </div>
-          )}
+    <>
+      <div className={style.box}>
+        <div className={style.selectedWeaponBox}>
+          {selectedWeaponAdvanceSetting !== undefined && 
+            <TableCheckCard 
+            weaponAdvanceSetting={selectedWeaponAdvanceSetting}
+            dispatchSetState={setSelectedWeaponAdvanceSetting}
+            />
+          }
         </div>
-        <div className={style.tableInputPortalButton}>
-          asdf
+        <div className={style.tableRecordInputSelectBox}>
+          <div className={style.weaponListbox}>
+            {WEAPON_LIST.map((weapon, index) => 
+              <div 
+                className={`${style.weaponButton} ${weaponList.includes(weapon) ? style.advanceSet : style.advanceUnSet} ${selectedWeaponAdvanceSetting?.weapon === weapon && style.selectedWeapon}`} 
+                key={`record-select-button-${weapon}-${index}`}
+                onClick={() => setSelectedWeaponAdvanceSetting(prev => {
+                  if (weaponAdvanceSetting[weapon] === undefined) return prev;
+                  if (prev?.weapon === weapon) return undefined;
+                  return {
+                    weapon : weapon,
+                    advanceSetting: weaponAdvanceSetting[weapon]
+                  }
+                })}
+              >
+                {weapon}
+              </div>
+            )}
+          </div>
+          <div className={style.tableInputPortalButton} onClick={() => setIsTableInputVisible(true)}>
+            직접 입력
+          </div>
         </div>
       </div>
-      
-    </div>
+      {isTableInputVisible && <VerifiedTableInputPortal dispatchFn={setIsTableInputVisible}/>}
+    </>
   )
 }
